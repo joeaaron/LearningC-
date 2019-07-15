ce27ef48508fba77f31970b3485d67300147a68a
joeaaron <pant333@163.com>
2019-07-15 17:00:30 +0800

    add two numbers----readme

--- LeetCode/AddTwoNumbers/solution.cpp
+++ LeetCode/AddTwoNumbers/solution.cpp
@@ -17,8 +17,8 @@ public:
 		for (div_t sum{ 0, 0 }; sum.quot || l1 || l2; tail = tail->next) {
 			if (l1) { sum.quot += l1->val; l1 = l1->next; }		// 进位 + l1 
 			if (l2) { sum.quot += l2->val; l2 = l2->next; }		// 进位 + l1 + l2
-			sum = div(sum.quot, 10);				 // 除 10 操作，得到新的 quot 与 rem.
-			tail->next = new ListNode(sum.rem);		 // rem 为节点值, quot 留作下次迭代
+			sum = div(sum.quot, 10);							// 除 10 操作，得到新的 quot 与 rem.
+			tail->next = new ListNode(sum.rem);					// rem 为节点值, quot 留作下次迭代
 		}
 		return dummy.next;
 	}
