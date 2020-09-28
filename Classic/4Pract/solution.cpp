9e16c8cb0808f6a952816c009251b61d9c814d68
P03918 <taopan@lusterinc.com>
2020-09-28 16:44:41 +0800

    字符串类：默认、移动、拷贝构造函数

--- Classic/4Pract/solution.cpp
+++ Classic/4Pract/solution.cpp
@@ -1,19 +1,37 @@
 #include "BigInteger.h"
+#include "strings.h"
+//TEST BIGINTEGER
+//int main()
+//{
+//	string s[2];
+//	cin >> s[0] >> s[1];
+//	BigInteger a[2];
+//	for (int i = 0; i < 2; i++) {
+//		a[i] = s[i];
+//	}
+//	BigInteger c = a[0] * a[1];
+//	for (int i = c.s.size() - 1; i >= 0; i--) {
+//		cout << c.s[i];
+//	}
+//	cout << endl;
+//	system("pause");
+//	return 0;
+//
+//}
 
+//TEST STRINGS
 int main()
 {
-	string s[2];
-	cin >> s[0] >> s[1];
-	BigInteger a[2];
-	for (int i = 0; i < 2; i++) {
-		a[i] = s[i];
-	}
-	BigInteger c = a[0] * a[1];
-	for (int i = c.s.size() - 1; i >= 0; i--) {
-		cout << c.s[i];
-	}
-	cout << endl;
+	String str1("Hello");
+	cout << str1.data() << '\n';
+	cout << str1.length() << '\n';
+	cout << "----------\n";
+	String str2 = "Word";
+	cout << str2 << '\n';
+	cout << "----------\n";
+	String str3 = str1 + str2;
+	cout << str3.data() << '\n';
+	cout << str3.length() << '\n';
 	system("pause");
 	return 0;
-
 }
