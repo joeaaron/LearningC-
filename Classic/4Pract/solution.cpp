553f24059f23c17d2f1e8d15fc3d6a5241b1aa24
P03918 <taopan@lusterinc.com>
2020-09-29 16:57:14 +0800

    简化的vector<T> 类模板

--- Classic/4Pract/solution.cpp
+++ Classic/4Pract/solution.cpp
@@ -1,26 +1,25 @@
 #include "BigInteger.h"
 #include "strings.h"
+#include "vectors.h"
 //TEST BIGINTEGER
-//int main()
-//{
-//	string s[2];
-//	cin >> s[0] >> s[1];
-//	BigInteger a[2];
-//	for (int i = 0; i < 2; i++) {
-//		a[i] = s[i];
-//	}
-//	BigInteger c = a[0] * a[1];
-//	for (int i = c.s.size() - 1; i >= 0; i--) {
-//		cout << c.s[i];
-//	}
-//	cout << endl;
-//	system("pause");
-//	return 0;
-//
-//}
+void TEST1()
+{
+	string s[2];
+	cin >> s[0] >> s[1];
+	BigInteger a[2];
+	for (int i = 0; i < 2; i++) {
+		a[i] = s[i];
+	}
+	BigInteger c = a[0] * a[1];
+	for (int i = c.s.size() - 1; i >= 0; i--) {
+		cout << c.s[i];
+	}
+	cout << endl;
+
+}
 
 //TEST STRINGS
-int main()
+void TEST2()
 {
 	String str1("Hello");
 	cout << str1.data() << '\n';
@@ -32,6 +31,52 @@ int main()
 	String str3 = str1 + str2;
 	cout << str3.data() << '\n';
 	cout << str3.length() << '\n';
+
+}
+
+//TEST VECTORS
+void TEST3()
+{
+	Vector<int> v(3, 1);
+	v.print();
+	cout << "size: " << v.size << " capacity: " << v.capacity << endl;
+	for (int iter = 0; iter < 11; iter++)
+	{
+		v.push_back(iter);
+	}
+	v.print();
+	v.pop_back();
+	v.pop_back();
+	v.pop_back();
+	v.print();
+	v[3] = 10086;
+	v.print();
+
+	cout << "what" << endl;
+	Vector<int> v2(v);
+	v2.print();
+
+	Vector<int> v3 = v;
+	v3.print();
+
+	Vector<int> v4;
+	v4 = v;
+	v4.print();
+
+	cout << "size: " << v4.size << " capacity: " << v4.capacity << endl;
+	v4.insert(1, 11111);
+	v4.insert(0, 22222);
+	v4.insert(v4.size - 1, 33333);
+	v4.insert(v4.size, 44444);
+	v4.print();
+	v4.clear();
+	v4.print();
+
+}
+
+int main()
+{
+	TEST3();
 	system("pause");
 	return 0;
 }
