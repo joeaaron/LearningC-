468e838c3a1d34548fc78bc3379c699d1f63b665
Stephen <pant333@163.com>
2024-05-13 09:21:38 +0800

    加入最小二乘平面拟合和RANSAC拟合

--- PCL/cloudPlane/CloudPlane.cpp
+++ PCL/cloudPlane/CloudPlane.cpp
@@ -7,32 +7,19 @@
 #include <pcl/sample_consensus/sac_model_plane.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <boost/thread/thread.hpp>
+#include <pcl/common/eigen.h>
+#include <pcl/common/centroid.h>
 
 using namespace std;
 
-int
-main(int argc, char** argv)
+void RANSCAPlaneFit(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
 {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-
-	if (pcl::io::loadPCDFile<pcl::PointXYZ>("sac_plane_test.pcd", *cloud) == -1)
-	{
-		PCL_ERROR("点云读取失败 \n");
-		return (-1);
-	}
-
-	//if (pcl::io::loadPLYFile<pcl::PointXYZ>("Scan_0511_1713.ply", *cloud) == -1)
-	//{
-	//	PCL_ERROR("点云读取失败 \n");
-	//	return (-1);
-	//}
-
     //------------------------------------------RANSAC框架--------------------------------------------------------   
     pcl::SampleConsensusModelPlane<pcl::PointXYZ>::Ptr model_plane(new pcl::SampleConsensusModelPlane<pcl::PointXYZ>(cloud));
 
     pcl::RandomSampleConsensus<pcl::PointXYZ> ransac(model_plane);//定义RANSAC算法模型
     ransac.setDistanceThreshold(0.01);//设定距离阈值
-    ransac.setMaxIterations(500);     //设置最大迭代次数
+    ransac.setMaxIterations(100);     //设置最大迭代次数
     ransac.setProbability(0.99);      //设置从离群值中选择至少一个样本的期望概率
     ransac.computeModel();            //拟合平面
     vector<int> inliers;              //用于存放内点索引的vector
@@ -80,8 +67,8 @@ main(int argc, char** argv)
     pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> after_sac(sac_plane, 0, 0, 255);
 
     viewer->addPointCloud(cloud, color, "cloud", v1);
-    viewer->addPointCloud(sac_plane, after_sac, "plane cloud", v2);
-    /*
+    //viewer->addPointCloud(sac_plane, after_sac, "plane cloud", v2);
+
     // 显示拟合出来的平面
     pcl::ModelCoefficients plane;
     plane.values.push_back(coeff[0]);
@@ -89,14 +76,84 @@ main(int argc, char** argv)
     plane.values.push_back(coeff[2]);
     plane.values.push_back(coeff[3]);
 
-    viewer->addPlane(plane, "plane",v2);
-    */
+    viewer->addPlane(plane, "cloud", v2);
+
 
     while (!viewer->wasStopped())
     {
         viewer->spinOnce(100);
         //boost::this_thread::sleep(boost::posix_time::microseconds(10000));
     }
+}
+
+void LeastSquareFit(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
+{
+	Eigen::Vector4d centroid;                    // 质心
+	Eigen::Matrix3d covariance_matrix;           // 协方差矩阵
+
+	// 计算归一化协方差矩阵和质心
+	pcl::computeMeanAndCovarianceMatrix(*cloud, covariance_matrix, centroid);
+
+	// 计算协方差矩阵的特征值与特征向量
+	Eigen::Matrix3d eigenVectors;
+	Eigen::Vector3d eigenValues;
+	pcl::eigen33(covariance_matrix, eigenVectors, eigenValues);
+
+	// 查找最小特征值的位置
+	Eigen::Vector3d::Index minRow, minCol;
+	eigenValues.minCoeff(&minRow, &minCol);
+
+	// 获取平面方程：AX+BY+CZ+D = 0的系数
+	Eigen::Vector3d normal = eigenVectors.col(minCol);
+	double D = -normal.dot(centroid.head<3>());
+
+	cout << "平面模型系数为：\n"
+		<< "A=" << normal[0] << "\n"
+		<< "B=" << normal[1] << "\n"
+		<< "C=" << normal[2] << "\n"
+		<< "D=" << D << "\n" << endl;
+
+	// 显示拟合出来的平面
+	pcl::ModelCoefficients plane;
+	plane.values.push_back(normal[0]);
+	plane.values.push_back(normal[1]);
+	plane.values.push_back(normal[2]);
+	plane.values.push_back(D);
+
+	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer("cloud show"));
+	int v1 = 0;
+
+	viewer->createViewPort(0, 0, 0.5, 1, v1);
+	viewer->setBackgroundColor(0, 0, 0, v1);
+
+	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color(cloud, 0, 255, 0);
+	viewer->addPlane(plane, "cloud", v1);
+
+	while (!viewer->wasStopped())
+	{
+		viewer->spinOnce(100);
+		//boost::this_thread::sleep(boost::posix_time::microseconds(10000));
+	}
+}
+
+int
+main(int argc, char** argv)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>("sac_plane_test.pcd", *cloud) == -1)
+	{
+		PCL_ERROR("点云读取失败 \n");
+		return (-1);
+	}
+
+	//if (pcl::io::loadPLYFile<pcl::PointXYZ>("Scan_0511_1713.ply", *cloud) == -1)
+	//{
+	//	PCL_ERROR("点云读取失败 \n");
+	//	return (-1);
+	//}
+    //RANSCAPlaneFit(cloud);
+    LeastSquareFit(cloud);
 
     return 0;
 }
